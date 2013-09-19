ab90246c583c0eb13b2e43267ce60cc9d0bdda07
Andrew Aitken <hermanthesheep@gmail.com>
2013-09-19 21:58:05 +0100

    Updated properties on photo model.

--- apps/photos/models.py
+++ apps/photos/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 
 class Photo(models.Model):
-    title = models.CharField(200)
-    image = models.ImageField()
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="uploads")
+    thumbnail = models.ImageField(upload_to="thumbnails")
