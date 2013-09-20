ddfed77b93bf6429f41f5f2f2b56803f734d6f62
Dougal Matthews <dougal85@gmail.com>
2013-09-20 11:59:27 +0100

    Added thumbnails and fixed issues with S3 storage.

--- apps/photos/models.py
+++ apps/photos/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.dispatch import receiver
+
+from easy_thumbnails.signals import saved_file
+from easy_thumbnails.files import generate_all_aliases
 
 
 class Photo(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to="uploads")
-    thumbnail = models.ImageField(upload_to="thumbnails")
+
+
+@receiver(saved_file)
+def generate_thumbnails_async(sender, fieldfile, **kwargs):
+    print "Generating thumbnail."
+    instance = sender._default_manager.get(pk=fieldfile.instance.pk)
+    fieldfile = getattr(instance, fieldfile.field.name)
+    generate_all_aliases(fieldfile, include_global=True)
