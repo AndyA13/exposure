edc1c9e5c5ba17d58f4fbcf6600320e2a085b8b6
Dougal Matthews <dougal85@gmail.com>
2013-09-20 12:50:16 +0100

    clarified directory names for static and media files.

--- exposure/settings.py
+++ exposure/settings.py
@@ -3,7 +3,8 @@ from __future__ import absolute_import
 from os import environ, path
 from sys import path as syspath
 
-syspath.append(path.dirname(__file__) + "/../apps/")
+PROJECT_DIRECTORY = path.abspath(path.dirname(__file__)) + "/.."
+syspath.append(path.join(PROJECT_DIRECTORY, "apps/"))
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -55,18 +56,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = path.join(PROJECT_DIRECTORY, "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = path.join(PROJECT_DIRECTORY, "static")
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -159,7 +160,8 @@ THUMBNAIL_ALIASES = {
     '': {
         'small-photo': {'size': (100, 100), 'crop': True},
         'medium-photo': {'size': (500, 500), 'crop': True},
-        'large-photo': {'size': (1000, 1000), 'crop': True}
+        'large-photo': {'size': (1000, 1000)},
+        'huge-photo': {'size': (2000, 2000)}
     },
 }
 
