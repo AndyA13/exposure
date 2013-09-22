3725947554b50abb7e0a8febb5ee286330629ba0
Andrew Aitken <hermanthesheep@gmail.com>
2013-09-22 21:41:03 +0100

    Added templates for set and photo detail page.

--- apps/ui/urls.py
+++ apps/ui/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'', 'ui.views.home')
+    url(r'^$', 'ui.views.home'),
+    url(r'^set/$', 'ui.views.photoset'),
+    url(r'^photo/$', 'ui.views.photo')
 )
