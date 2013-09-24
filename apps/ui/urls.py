98c37c583dd0adfcb544c20bcae1f9949bb6a207
Andrew Aitken <hermanthesheep@gmail.com>
2013-09-24 22:45:56 +0100

    Added header link and removed test urls.

--- apps/ui/urls.py
+++ apps/ui/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'ui.views.home'),
-    url(r'^set/$', 'ui.views.photoset'),
-    url(r'^photo/$', 'ui.views.photo')
+    url(r'', 'ui.views.home')
 )
