c2a77b93cbd0f3682862a42f73aec5aadd993298
Andrew Aitken <hermanthesheep@gmail.com>
2013-11-06 20:51:19 +0000

    Adding new urls.

--- apps/ui/urls.py
+++ apps/ui/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('ui.views',
     url(r'^$', 'home', name="home"),
-    url(r'^(?P<photo_set_slug>[\w-]+)/$', 'photo_set', name="photo_set"),
-    url(r'^(?P<photo_set_slug>[\w-]+)/(?P<photo_slug>[\w-]+)/$', 'photo_detail', name="photo_detail"),
+    url(r'^sets/(?P<photo_set_slug>[\w-]+)/$', 'photo_set', name="photo_set"),
+    url(r'^sets/(?P<photo_set_slug>[\w-]+)/(?P<photo_slug>[\w-]+)/$', 'photo_detail', name="photo_detail"),
+    url(r'^photos/(?P<photo_slug>[\w-]+)/$', 'photo_detail', name="photo_detail"),
 )
